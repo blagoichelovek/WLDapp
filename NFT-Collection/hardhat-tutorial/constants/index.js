4c22d42c46effb60deed13b4f58712b347b8126a
Zakhar Nechepurenko <comfy798@gmail.com>
2023-02-02 19:43:56 +0200

    Updating with metadata

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x611f599d5aDC96a6C92cFc4938676428A4ecFB80"
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://wl-dapp-c9zm.vercel.app/api/";
 module.exports = {
     WHITELIST_CONTRACT_ADDRESS,
     METADATA_URL
